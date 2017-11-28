6fe605eeb871db791755486b453ca9b0b363d8c6
cosminchiriac97 <chiriac.cosmin97@gmail.com>
2017-11-28 19:01:16 +0200

    Added Layer reprezentation + Prezentation Project

---
+++ FiiAdmission/FiiAdmissionPrezentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FiiAdmissionPrezentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
