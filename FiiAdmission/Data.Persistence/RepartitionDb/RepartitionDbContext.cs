1664eb9838d284e22d9a3a3fd6ce3eb499576a0e
andreichitic <andrei.chitic_22@yahoo.com>
2018-01-14 17:44:28 +0200

    Repartition
    Repartition +Repository

---
+++ FiiAdmission/Data.Persistence/RepartitionDb/RepartitionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Persistence.RepartitionDb
+{
+   public class RepartitionDbContext: DbContext, IRepartitionDbContext
+    {
+        public RepartitionDbContext(DbContextOptions<RepartitionDbContext> options) : base((DbContextOptions) options)
+        {
+
+        }
+
+        public DbSet<Repartition> Repartitions { get; set; }
+    }
+}
