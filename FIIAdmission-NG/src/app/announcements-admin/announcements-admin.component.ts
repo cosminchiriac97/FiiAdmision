3dbd5aabb3d02081dcbfa32761e2173fdb1e574b
Giosanu <giosanu.george@gmail.com>
2018-01-16 14:06:46 +0200

    ----

---
+++ FIIAdmission-NG/src/app/announcements-admin/announcements-admin.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { AnnouncementsService } from '../_services/announcements.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-announcements-admin',
+  templateUrl: './announcements-admin.component.html',
+  styleUrls: ['./announcements-admin.component.css']
+})
+export class AnnouncementsAdminComponent implements OnInit {
+
+  announcementId: string;
+  model: any = {};
+  model2: any = {};
+
+  constructor(
+    private router: Router,
+    private announcementsService: AnnouncementsService) { }
+
+  ngOnInit() {
+  }
+
+
+  getAnnouncement(id) {
+    this.announcementsService.getAnnouncement(id)
+      .subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        alert(error);
+      });
+  }
+
+  addAnnouncement() {
+    this.model.adminId = 'c4e3c477-eff5-45b5-a4c6-2d6d28eae60d';
+    this.model.publishDate = new Date().toJSON();
+    this.announcementsService.addAnnouncement(this.model)
+      .subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  updateAnnouncement() {
+    this.model2.adminId = 'c4e3c477-eff5-45b5-a4c6-2d6d28eae60d';
+    this.model2.publishDate = new Date().toJSON();
+    this.announcementsService.updateAnnouncement(this.model2)
+      .subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+  deleteAnnouncement(id) {
+    this.announcementsService.deleteAnnouncement(id).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+}
--- FIIAdmission-NG/src/app/sidenav/sidenav.component.ts
+++ FIIAdmission-NG/src/app/sidenav/sidenav.component.ts
@@ -34,56 +34,48 @@ export class SidenavComponent {
     this.router.navigate(['/']);
   }
 
-  news() {
-    this.toggle(1, 0, 0, 0, 0, 0);
+  announcements() {
+    this.toggle(1, 0, 0, 0, 0);
   }
 
   notifications() {
-    this.toggle(0, 1, 0, 0, 0, 0);
+    this.toggle(0, 1, 0, 0, 0);
   }
 
-  announcements(){
-    this.toggle(0, 0, 1, 0, 0, 0)
-  }
   form() {
-    this.toggle(0, 0, 0, 1, 0, 0);
+    this.toggle(0, 0, 1, 0, 0);
   }
 
   documents() {
-    this.toggle(0, 0, 0, 0, 1, 0);
+    this.toggle(0, 0, 0, 1, 0);
   }
 
   reset() {
-    this.toggle(0, 0, 0, 0, 0, 1);
+    this.toggle(0, 0, 0, 0, 1);
   }
 
-  toggle(a, b, c, d, e, f) {
+  toggle(a, b, c, d, e) {
     if (a === 1) {
-      document.getElementById('news').style.display = 'block';
+      document.getElementById('announcements').style.display = 'block';
     } else {
-      document.getElementById('news').style.display = 'none';
+      document.getElementById('announcements').style.display = 'none';
     }
     if (b === 1) {
       document.getElementById('notifications').style.display = 'block';
     } else {
       document.getElementById('notifications').style.display = 'none';
     }
-    if (c == 1){
-      document.getElementById('announcements').style.display = 'block';
-    } else {
-      document.getElementById('announcements').style.display = 'none';
-    }
-    if (d === 1) {
+    if (c === 1) {
       document.getElementById('form').style.display = 'block';
     } else {
       document.getElementById('form').style.display = 'none';
     }
-    if (e === 1) {
+    if (d === 1) {
       document.getElementById('documents').style.display = 'block';
     } else {
       document.getElementById('documents').style.display = 'none';
     }
-    if (f === 1) {
+    if (e === 1) {
       document.getElementById('reset').style.display = 'block';
     } else {
       document.getElementById('reset').style.display = 'none';
