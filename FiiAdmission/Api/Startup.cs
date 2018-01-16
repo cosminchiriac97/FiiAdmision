a694077b8f8c04429c3db19449152dfd7469d118
Domnaru Alexandru <domnaru.alexandru@gmail.com>
2018-01-16 18:38:45 +0200

    MORE ISSUES

--- FiiAdmission/Api/Startup.cs
+++ FiiAdmission/Api/Startup.cs
@@ -43,14 +43,12 @@ namespace Api
 
     public IConfiguration Configuration { get; }
 
-<<<<<<< HEAD
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddTransient<IApplicationUserDbContext, ApplicationUserDbContext>();
       services.AddTransient<IEmailSender, EmailSender>();
       services.AddTransient<IAnnouncementsRepository, AnnouncementsRepository>();
-      services.AddTransient<IFormRepository, FormRepository>();
       services.AddTransient<IJobSeekerRepository, JobSeekerRepository>();
       services.AddTransient<IContentDbContext, ContentDbContext>();
       services.AddSingleton<IJwtFactory, JwtFactory>();
@@ -86,7 +84,7 @@ namespace Api
         ValidateLifetime = true,
         ClockSkew = TimeSpan.Zero
       };
- 
+
       services.AddAuthentication(options =>
       {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -117,111 +115,6 @@ namespace Api
       */
       services.AddIdentity<AppUser, IdentityRole>
         (o =>
-=======
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddTransient<IApplicationUserDbContext, ApplicationUserDbContext>();
-            services.AddTransient<IEmailSender, EmailSender>();
-            services.AddTransient<IAnnouncementsRepository, AnnouncementsRepository>();
-            services.AddTransient<IJobSeekerRepository, JobSeekerRepository>();
-            services.AddTransient<IContentDbContext, ContentDbContext>();
-            services.AddTransient<IGenerateCandidate,GenerateCandidate>();
-            services.AddTransient<ICandidateRepository, CandidateRepository>();
-
-            services.AddDbContext<ContentDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("ContentDbConnection")));
-
-            services.AddDbContext<ApplicationUserDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("AppUsersConnection")));
-
-            // ===== Add Jwt Authentication ========
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
-
-            services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-                })
-                .AddJwtBearer(cfg =>
-                {
-                    cfg.RequireHttpsMetadata = false;
-                    cfg.SaveToken = true;
-                    cfg.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidIssuer = Configuration["JwtIssuer"],
-                        ValidAudience = Configuration["JwtIssuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
-                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
-                    };
-                });
-            /*
-            services.Configure<MvcOptions>(options =>
-            {
-                options.Filters.Add(new RequireHttpsAttribute());
-            });
-            */
-            services.AddIdentity<AppUser, IdentityRole>
-                (o =>
-                {
-      
-                    o.Password.RequireDigit = false;
-                    o.Password.RequireLowercase = false;
-                    o.Password.RequireUppercase = false;
-                    o.Password.RequireNonAlphanumeric = false;
-                    o.Password.RequiredLength = 6;
-
-                    //o.SignIn.RequireConfirmedEmail = true;
-                })
-                .AddEntityFrameworkStores<ApplicationUserDbContext>()
-                .AddDefaultTokenProviders();
-
-          
-
-            services.AddScoped<IAzureBlobStorage>(factory =>
-            {
-                return new AzureBlobStorage(new AzureBlobSetings(
-                    storageAccount: Configuration["Blob_StorageAccount"],
-                    storageKey: Configuration["Blob_StorageKey"],
-                    containerName: Configuration["Blob_ContainerName"]));
-            });
-
-            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader();
-            }));
-            // Add application services.
-           
-            services.AddMvc().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
-            
-            services.AddAutoMapper();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info { Title = "FIIAdmission API", Version = "v1" });
-            });
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("Administrator", policy => policy.RequireClaim("Admin"));
-                options.AddPolicy("User", policy => policy.RequireClaim("User"));
-            });
-
-            services.AddSignalR();
-
-            services.AddMvc();
-            
-    
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
->>>>>>> fe6e0f14d8d13cd99468749ac6ee0e2302b4894f
         {
           // configure identity options
           o.Password.RequireDigit = false;
@@ -297,7 +190,7 @@ namespace Api
       });
       app.UseDefaultFiles();
       app.UseStaticFiles();
-      
+
       app.UseMvc();
     }
   }
